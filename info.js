473a474fecc13ced4e5b095c4e64e6a8f7a46f53
donaldtrumprules <mkolesnikov.sh@mysummitps.org>
2016-03-22 20:17:01 +0000

    update

--- info.js
+++ info.js
@@ -1,15 +1,22 @@
 var item1 = document.getElementById("item1");
 var item2 = document.getElementById("item2");
+var item3 = document.getElementById("item3");
+var item4 
 var info1 = document.getElementById("info1");
 var info2 = document.getElementById("info2");
+var info3 = document.getElementById("info3");
 
-item1.addEventListener("click", function(){
-    info1.innerHTML = "Some stuff";
-    info2.innerHTML = "some other stuff";
+item1.addEventListener ("click", function(){
+    info1.innerHTML = "Stephen Curry splash zone";
+    info2.innerHTML = "Stephen Curry doing a layup on a dock";
+    info3.innerHTML = "Stephen Cuury dribbling on a dock";
+    info4.innerHTML = "stephen Curry shooting a 3 pointer over JJ. Redick";
 });
                        
 item2.addEventListener("click", function(){
     info1.innerHTML = "Item 2 stuff";
     info2.innerHTML = "Item 2 other stuff";
 });
+                       
+                       
                        
